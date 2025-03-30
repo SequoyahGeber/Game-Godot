6b51b50bb1604e1c7d28ea861fce892aca7aba3d
SequoyahGeber <sequoyahgeber@gmail.com>
2025-03-30 11:52:55 -0700

    coin collision detection

---
+++ scripts/coin.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+func _ready() -> void:
+	# on the target object (in this case, 'self' refers to this Area2D node).
+	var body_entered_callable = Callable(self, "_on_body_entered")
+
+	if not is_connected("body_entered", body_entered_callable):
+		connect("body_entered", body_entered_callable)
+
+	
+	print(self.name + " (Area2D) is ready to detect collisions.")
+
+
+func _on_body_entered(body: PhysicsBody2D):
+	print(self.name + " detected collision with body: " + body.name)
+
+
+func _process(_delta: float) -> void:
+	pass
